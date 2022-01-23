e196ca538c2fa2f6728f3fa6981e6556ae6688c8
Ivan Stasiuk <brokeyourbike@gmail.com>
2022-01-23 13:44:53 +0200

    chore: add cprt

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,11 @@
 <?php
 
+// Copyright (C) 2022 Ivan Stasiuk <ivan@stasi.uk>.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 namespace BrokeYourBike\UidKeys\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
